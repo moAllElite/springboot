eeaba7f2b5e73ec020fb1640379c6afc42994504
moAllElite <mouhamedn347@gmail.com>
2023-02-24 00:15:50 +0000

    Project EcobankApplication from udemy full stack spring angular

--- src/main/java/com/groupe/ecobank/repositories/AccountRepository.java
+++ src/main/java/com/groupe/ecobank/repositories/AccountRepository.java
@@ -13,4 +13,6 @@ public interface AccountRepository extends JpaRepository<Account,Integer> {
     Account searchByIbanContainsIgnoreCase(String iban);
     @Query("from  Account a inner join  User u where u.id=a.user.id and a.creationDate=:creationDate ")
     Account searchByCreationDate(Date creationDate );
+    @Query(value = "select  * from account where  id=:id group by id",nativeQuery = true)
+    Account searchByIdNative(String id);
 }
