e183d447f5ed318840e20254ebc7d44ef4a90f41
moAllElite <mouhamedn347@gmail.com>
2023-04-02 01:17:32 +0000

    spring boot angular  ecobank une application bancaire complete avec un Dashboard et graphes

---
+++ src/main/java/com/groupe/ecobank/controller/AddressController.java
@@ -0,0 +1,40 @@
+package com.groupe.ecobank.controller;
+
+import com.groupe.ecobank.dto.AddressDto;
+import com.groupe.ecobank.service.IAddressService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/addresses")
+@RequiredArgsConstructor
+public class AddressController {
+    private  final IAddressService service;
+
+    @GetMapping("/")
+    public ResponseEntity<Integer> save(
+         @RequestBody AddressDto addressDto
+    ){
+        return ResponseEntity.ok(service.save(addressDto));
+    }
+    @GetMapping("/")
+    public ResponseEntity <List<AddressDto>> findAll(){
+        return  ResponseEntity.ok(service.findAll());
+    }
+    @GetMapping("/{address-id}")
+    public ResponseEntity <AddressDto> findById(
+            @PathVariable Integer addressId
+    ){
+        return  ResponseEntity.ok(service.findById(addressId));
+    }
+    @DeleteMapping("/{address-id}")
+    public ResponseEntity<Void> delete(
+            @PathVariable Integer addressId
+    ){
+        service.delete(addressId);
+        return ResponseEntity.accepted().build();
+    }
+}
---
+++ src/main/java/com/groupe/ecobank/controller/UserController.java
@@ -0,0 +1,63 @@
+package com.groupe.ecobank.controller;
+
+import com.groupe.ecobank.dto.UserDto;
+import com.groupe.ecobank.service.IUserService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+/**
+ * pour le request mapping convention
+ * nom de la ressource au pluriel ex:users
+ * readabelaté pas de tiret de 8 ou de camel case
+ */
+@RequestMapping("/users")
+@RequiredArgsConstructor
+public class UserController {
+    private final IUserService service;
+
+    /**
+     * pour enregistrer un user
+     * @param userDto
+     * @return
+     */
+    @PostMapping("/")
+    public ResponseEntity<Integer>save(
+         @RequestBody   UserDto userDto
+    ){
+        return  ResponseEntity.ok(service.save(userDto));
+    }
+    @GetMapping("/")
+    public ResponseEntity<List<UserDto>> findAll(){
+        return  ResponseEntity.ok(service.findAll());
+    }
+    @GetMapping("/{user-id}")
+    public ResponseEntity<UserDto> findById(
+          @PathVariable("user-id")  Integer userId
+    ){
+        return ResponseEntity.ok(service.findById(userId));
+    }
+    @PatchMapping("/validate/{user-id}")
+    public ResponseEntity<Integer> validateAccount(
+            @PathVariable("user-id") Integer userId
+    ){
+        return  ResponseEntity.ok(service.validateAccount(userId));
+    }
+    @PatchMapping("/invalidate/{user-id}")
+    public ResponseEntity<Integer> invalidateAccount(
+            @PathVariable("user-id") Integer userId
+    ){
+        return ResponseEntity.ok(service.invalideAccount(userId));
+    }
+    @DeleteMapping("/{user-id}")
+    public  ResponseEntity<Void> delete(
+            @PathVariable("user-id") Integer userId
+    ){
+       service.delete(userId);
+       return ResponseEntity.accepted().build();
+    }
+
+}
