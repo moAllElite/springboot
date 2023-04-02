8544137be3b6589bb332325032df9cb2b63417f7
moAllElite <mouhamedn347@gmail.com>
2023-04-02 00:10:46 +0000

    spring boot angular  ecobank une application bancaire complete avec un Dashboard et graphes

--- src/main/java/com/groupe/ecobank/handlers/ExceptionRespresentation.java
+++ src/main/java/com/groupe/ecobank/handlers/ExceptionRespresentation.java
@@ -1,13 +1,14 @@
 package com.groupe.ecobank.handlers;
 
 import com.fasterxml.jackson.annotation.JsonInclude;
+import com.fasterxml.jackson.annotation.JsonInclude.Include;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 
 import java.util.Set;
 @Builder
 @AllArgsConstructor
-@JsonInclude(JsonInclude.Include.NON_EMPTY)
+@JsonInclude(Include.NON_EMPTY)
 public class ExceptionRespresentation {
     private  String erroMessage;
     private String errorSource;
